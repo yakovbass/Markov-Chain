6e8ca8e57529ee699b2a0bafc661492854f3ce19
Yakov Bass <Yakov.Bass@mail.huji.ac.il>
2025-03-23 19:23:47 +0200

    my commit

--- Markov Chain/markov_chain.c
+++ Markov Chain/markov_chain.c
@@ -1,23 +1,46 @@
 #include "markov_chain.h"
 #include <string.h>
+
+#define TRAVELLER 0
+#define FLAG 1
+#define WHICH_WORD 0
+#define LEN_OF_TWEET 0
+#define DELIMITERS " \n\t\r"
+#define MAX_LEN_ROW 1000
+#define START_CHAIN 0
+#define LEN_OF_TWEETS 1
+#define MAX_LEN_OF_TWEET 20
 /**
  * Get random number between 0 and max_number [0, max_number).
  * @param max_number
  * @return Random number
  */
-#define TRAVELLER 0
-#define FLAG 1
-#define WHICH_WORD 0
-#define LEN_OF_TWEET 1
-
-int get_random_number(int max_number)
-{
+int get_random_number(int max_number) {
     return rand() % max_number;
 }
-Node* get_node_from_database(MarkovChain *markov_chain, void *data_ptr){
-    Node * traveller = markov_chain->database->first;
-    while (traveller){
-        if (markov_chain->comp_func(traveller->data->data,data_ptr) == 0){
+
+MarkovChain * make_markov_chain() {
+    MarkovChain *markov_chain = (MarkovChain *) malloc(sizeof(MarkovChain));
+    if (markov_chain == NULL) {
+        fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
+        return NULL;
+    }
+    LinkedList *list = (LinkedList *) malloc(sizeof(LinkedList));
+    if (list == NULL) {
+        fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
+        return NULL;
+    }
+    list->first = NULL;
+    list->last = NULL;
+    list->size = 0;
+    markov_chain->database = list;
+    return markov_chain;
+}
+
+Node *get_node_from_database(MarkovChain *markov_chain, char *data_ptr) {
+    Node *traveller = markov_chain->database->first;
+    while (traveller) {
+        if (strcmp(traveller->data->data, data_ptr) == 0) {
             return traveller;
         }
         traveller = traveller->next;
@@ -25,66 +48,86 @@ Node* get_node_from_database(MarkovChain *markov_chain, void *data_ptr){
     return NULL;
 }
 
+char *my_strdup(const char *s) {
+    size_t len = strlen(s) + 1;
+    char *dup = malloc(len);
+    if (dup != NULL) {
+        strcpy(dup, s);
+    }
+    return dup;
+}
 
-Node* add_to_database(MarkovChain *markov_chain, void *data_ptr){
-    MarkovNode* new_markov_node
-    = (MarkovNode*) malloc(sizeof (MarkovNode));
-    if (new_markov_node == NULL){
-        fprintf(stdout,ALLOCATION_ERROR_MASSAGE);
+Node *add_to_database(MarkovChain *markov_chain, char *data_ptr) {
+    MarkovNode *new_markov_node = (MarkovNode *) malloc(sizeof(MarkovNode));
+    if (new_markov_node == NULL) {
+        fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
         return NULL;
     }
-//    new_markov_node->data = my_strdup(markov_chain,data_ptr);
-    new_markov_node->data
-    = markov_chain->copy_func(data_ptr);
-    if (new_markov_node->data == NULL){
+    new_markov_node->data = my_strdup(data_ptr);
+    if (new_markov_node->data == NULL) {
+        free(new_markov_node);
         return NULL;
     }
     new_markov_node->frequency_list = NULL;
     new_markov_node->following_count = 0;
-    Node *new_node =
-            (Node *)malloc(sizeof(Node));
-    if (new_node == NULL){
-        fprintf(stdout,ALLOCATION_ERROR_MASSAGE);
+    int addNode = add(markov_chain->database, new_markov_node);
+    if (addNode == 1) {
         return NULL;
     }
-    new_node->data = new_markov_node;
-    new_node->next = NULL;
-    if (markov_chain->database->first == NULL) {
-        // Adding the first node to the list
-        markov_chain->database->first = new_node;
-        markov_chain->database->last = new_node;
-    } else {
-        // Adding a new node at the end of the list
-        markov_chain->database->last->next = new_node;
-        markov_chain->database->last = new_node;
-    }
-    markov_chain->database->size++;
-    return new_node;
+    return markov_chain->database->last;
 }
 
-int new_frequency_list(MarkovNode *first_node
-                       , MarkovNode *second_node){
+
+int fill_database(FILE *fp, long words_to_read, MarkovChain *markov_chain) {
+    int start_chain = START_CHAIN;
+    char *row = malloc(MAX_LEN_ROW * sizeof(char));
+    if (row == NULL) {
+        fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
+        return EXIT_FAILURE;
+    }
+    MarkovNode *save_last_one = NULL;
+    while (fgets(row, MAX_LEN_ROW, fp)
+           != NULL && start_chain < words_to_read) {
+        char *token = strtok(row, DELIMITERS);
+        while (token && words_to_read != -1 && start_chain < words_to_read) {
+            Node *has_node = get_node_from_database(markov_chain, token);
+            if (has_node == NULL) {
+                has_node = add_to_database(markov_chain, token);
+            }
+            int add_to_frequency_list
+                    = add_node_to_frequency_list(save_last_one,
+                                                 has_node->data);
+            if (add_to_frequency_list == EXIT_FAILURE) {
+                return EXIT_FAILURE;
+            }
+            save_last_one = has_node->data;
+            token = strtok(NULL, DELIMITERS);
+            start_chain++;
+        }
+    }
+    free(row);
+    row = NULL;
+    return EXIT_SUCCESS;
+}
+
+int new_frequency_list(MarkovNode *first_node, MarkovNode *second_node) {
     first_node->frequency_list = (MarkovNodeFrequency *)
             malloc(sizeof(MarkovNodeFrequency));
-    if (first_node->frequency_list == NULL){
-        fprintf(stdout,ALLOCATION_ERROR_MASSAGE);
+    if (first_node->frequency_list == NULL) {
+        fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
         return EXIT_FAILURE;
     }
     first_node->frequency_list[0].markov_node = second_node;
     first_node->frequency_list[0].frequency = 1;
     first_node->following_count = 1;
-    first_node->frequency_list->num_of_nodes = 1;
     first_node->all_following = 1;
     return EXIT_SUCCESS;
 }
 
-int add_num_of_frequency(MarkovNode *first_node
-                         , MarkovNode *second_node
-                         , MarkovChain *markov_chain) {
+int add_num_of_frequency(MarkovNode *first_node, MarkovNode *second_node) {
     for (int i = 0; i < first_node->following_count; i++) {
-        if ((markov_chain->comp_func(
-                first_node->frequency_list[i].markov_node->data
-                ,second_node->data)) == 0) {
+        if (strcmp(first_node->frequency_list[i].markov_node->data,
+                   second_node->data) == 0) {
             first_node->frequency_list[i].frequency++;
             first_node->all_following++;
             return EXIT_SUCCESS;
@@ -93,33 +136,33 @@ int add_num_of_frequency(MarkovNode *first_node
     return EXIT_FAILURE;
 }
 
-int add_node_to_frequency_list(MarkovNode *first_node
-        , MarkovNode *second_node, MarkovChain *markov_chain){
+int add_node_to_frequency_list(MarkovNode *first_node, MarkovNode *second_node) {
+    if (first_node == second_node || first_node == NULL) {
+        return EXIT_SUCCESS;
+    }
     if (first_node->frequency_list == NULL) {
         int make_new_frequency_list
-                = new_frequency_list(first_node,second_node);
-        if (make_new_frequency_list == EXIT_FAILURE){
+                = new_frequency_list(first_node, second_node);
+        if (make_new_frequency_list == EXIT_FAILURE) {
             return EXIT_FAILURE;
         }
-    }
-    else{
+    } else {
         int add_num_to_frequency_list
-                = add_num_of_frequency(first_node,second_node, markov_chain);
-        if (add_num_to_frequency_list == EXIT_SUCCESS){
+                = add_num_of_frequency(first_node, second_node);
+        if (add_num_to_frequency_list == EXIT_SUCCESS) {
             return EXIT_SUCCESS;
         }
-        first_node->frequency_list->num_of_nodes++;
         MarkovNodeFrequency *new_list =
-                (MarkovNodeFrequency *)realloc
+                (MarkovNodeFrequency *) realloc
                         (first_node->frequency_list,
                          (first_node->following_count + 1) *
                          sizeof(MarkovNodeFrequency));
         if (new_list == NULL) {
-            fprintf(stdout,ALLOCATION_ERROR_MASSAGE);
+            fprintf(stdout, ALLOCATION_ERROR_MASSAGE);
             return EXIT_FAILURE; // Memory reallocation failed
         }
 
-        // Add the new node to the frequency list
+// Add the new node to the frequency list
         new_list[first_node->following_count].markov_node = second_node;
         new_list[first_node->following_count].frequency = 1;
         first_node->following_count++;
@@ -129,18 +172,18 @@ int add_node_to_frequency_list(MarkovNode *first_node
     return EXIT_SUCCESS;
 }
 
-MarkovNode* get_first_random_node(MarkovChain *markov_chain){
-    Node * node_to_return = markov_chain->database->first;
-    while (FLAG){
+MarkovNode *get_first_random_node(MarkovChain *markov_chain) {
+    Node *node_to_return = markov_chain->database->first;
+    while (FLAG) {
         int num = get_random_number(markov_chain->database->size);
         int traveller = TRAVELLER;
-        while (traveller != num){
+        while (traveller != num) {
             node_to_return = node_to_return->next;
             traveller++;
         }
-        void * one_word = node_to_return->data->data;
-        //unsigned  long len_word = strlen(one_word);
-        if (!(markov_chain->is_last(one_word))){
+        char *one_word = node_to_return->data->data;
+        unsigned long len_word = strlen(one_word);
+        if (one_word[len_word - 1] != '.') {
             break;
         }
         node_to_return = markov_chain->database->first;
@@ -148,9 +191,9 @@ MarkovNode* get_first_random_node(MarkovChain *markov_chain){
     return node_to_return->data;
 }
 
-MarkovNodeFrequency * which_node(MarkovNode *first_node, int random_num) {
+MarkovNodeFrequency *which_node(MarkovNode *first_node, int random_num) {
     int which_word = WHICH_WORD;
-    for (int i = 0; i < first_node->following_count; ++i) {
+    for (int i = 0; i < first_node->following_count; i++) {
         MarkovNodeFrequency *freq = &(first_node->frequency_list[i]);
         which_word = which_word + freq->frequency;
         if (which_word > random_num) {
@@ -161,44 +204,59 @@ MarkovNodeFrequency * which_node(MarkovNode *first_node, int random_num) {
     return freq;
 }
 
-MarkovNode* get_next_random_node(MarkovNode *cur_markov_node){
+void make_tweets(long max_tweets, MarkovChain *markov_chain) {
+    int num_tweets = LEN_OF_TWEETS;
+    while (num_tweets <= max_tweets) {
+        fprintf(stdout, "Tweet %d: ", num_tweets);
+        MarkovNode *first_node = get_first_random_node(markov_chain);
+        fprintf(stdout, "%s", first_node->data);
+        generate_tweet(first_node, MAX_LEN_OF_TWEET);
+        num_tweets++;
+        fprintf(stdout, "\n");
+    }
+}
+
+
+MarkovNode *get_next_random_node(MarkovNode *cur_markov_node) {
     int random_num = get_random_number(cur_markov_node->all_following);
-    MarkovNodeFrequency * freq_node = which_node(cur_markov_node,random_num);
+    MarkovNodeFrequency *freq_node = which_node(cur_markov_node, random_num);
     return freq_node->markov_node;
 }
 
-void generate_random_sequence(MarkovChain *markov_chain, MarkovNode *
-first_node, int max_length){
+
+void generate_tweet(MarkovNode *first_node, int max_length) {
     int len_of_tweet = LEN_OF_TWEET;
-    while ((!markov_chain->is_last(first_node->data))
-    && len_of_tweet < max_length){
+    while (first_node->data[strlen(first_node->data) - 1]
+           != '.' && len_of_tweet <= max_length) {
         first_node = get_next_random_node(first_node);
-        markov_chain->print_func(first_node->data);
+        fprintf(stdout, " %s", first_node->data);
         len_of_tweet++;
     }
 }
 
-void free_markov_chain(MarkovChain ** ptr_chain){
+
+void free_database(MarkovChain **ptr_chain) {
     if (ptr_chain == NULL || *ptr_chain == NULL) {
         return;
     }
     MarkovChain *chain = *ptr_chain;
-    if (chain->database == NULL){
+    if (chain->database == NULL) {
         free(chain);
         chain = NULL;
         return;
     }
     Node *traveller = chain->database->first;
-    while (traveller){
-        MarkovNode * our_node = traveller->data;
-        if (our_node){
-            if (our_node->data){
-                chain->free_data(our_node->data);
+    while (traveller) {
+        MarkovNode *our_node = traveller->data;
+
+        if (our_node) {
+            if (our_node->data) {
+                free(our_node->data);
                 our_node->data = NULL;
             }
-            if (our_node->frequency_list){
+            if (our_node->frequency_list) {
                 free(our_node->frequency_list);
-                our_node->frequency_list =NULL;
+                our_node->frequency_list = NULL;
             }
             free(our_node);
             our_node = NULL;
